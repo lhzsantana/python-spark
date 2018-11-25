8ea25f5ed49fc2610aed4fe6f0a578bf3c9e2ab4
luiz.santana <luiz@leafagriculture.com.br>
2018-11-25 16:39:37 -0200

    Fixed bugs

--- daily.py
+++ daily.py
@@ -1,7 +1,9 @@
 import redis
+import sys
 r = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 LAST_PREFIX="last"
+DAILY_PREFIX="daily"
 COUNTER_PREFIX="counter"
 WINDOW_PREFIX="window"
 LIST_PREFIX="list"
@@ -11,11 +13,15 @@ def control_window(clientIds):
 	for clientId in clientIds:
     		for gender in all_genders:
 			gender_sum=0
-			r.lpop(LIST_PREFIX+":"+WINDOW_PREFIX+":"+str(clientId)+":"+gender)
-			r.rpush(LIST_PREFIX+":"+WINDOW_PREFIX+":"+str(clientId)+":"+gender, r.get(WINDOW_PREFIX+":"+DAILY_PREFIX+":"+str(clientId)+":"+gender)))
-			for day in r.lrange(LIST_PREFIX+":"+WINDOW_PREFIX+":"+str(clientId)+":"+gender, 0, 7):
-				gender_sum+=day
-				r.set(WINDOW_PREFIX+":"+str(clientId)+":"+gender, gender_sum)
+			gender_value=0
+			key = WINDOW_PREFIX+":"+DAILY_PREFIX+":"+clientId+":"+gender
+			if r.exists(key):
+				gender_value= r.get(key)
+			r.lpop(LIST_PREFIX+":"+WINDOW_PREFIX+":"+clientId+":"+gender)
+			r.rpush(LIST_PREFIX+":"+WINDOW_PREFIX+":"+clientId+":"+gender, gender_value)
+			for day in r.lrange(LIST_PREFIX+":"+WINDOW_PREFIX+":"+clientId+":"+gender, 0, 7):
+				gender_sum+=int(day)
+				r.set(WINDOW_PREFIX+":"+clientId+":"+gender, gender_sum)
 
 if __name__ == "__main__":
 	control_window(sys.argv[1].split(","))
